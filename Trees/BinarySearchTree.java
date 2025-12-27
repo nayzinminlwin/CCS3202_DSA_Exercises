97cf3ad985765d2d2d50055246add4a2129aefa7
Nay Zin Min Lwin <nayzinminlwin22@gmail.com>
2025-12-28 01:13:37 +0800

    Added Tree Height Function, pre-fix, and post-fix traversal functions.

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -7,39 +7,36 @@ public class BinarySearchTree {
     public static void main(String[] args) {
         // 12, 22, 8, 19, 10, 9, 20, 4, 2, 6
         int[] intArr = { 12, 22, 8, 19, 10, 9, 20, 4, 2, 6 };
+        intArr = new int[] { 70, 60, 80, 50, 65, 75, 85, 45, 55, 90 };
+        intArr = new int[] { 14, 23, 7, 10, 33, 56, 80, 66, 70 };
+
         Node myTree = null;
 
         for (int i : intArr) {
-            if (myTree == null) {
-                myTree = new Node(i);
-            } else {
-                recursiveBinaryInsert(myTree, i);
-            }
+            myTree = Node.InsertNodeBST(myTree, i);
         }
 
         System.out.println("Original array : " + Arrays.toString(intArr));
-        System.out.print("Sorted array : ");
-        Node.inorder(myTree);
 
-    }
+        System.out.print("NLR : ");
+        Node.preOrderBST(myTree);
 
-    public static void recursiveBinaryInsert(Node currentNode, int newVal) {
-        if (newVal < currentNode.getCurrentNode()) {
-            if (currentNode.getLeftChild() == null) {
-                currentNode.addLeftChild(new Node(newVal, currentNode));
-                System.out.printf("%d inserted left to parent : %d\n", newVal, currentNode.getCurrentNode());
-            } else {
-                recursiveBinaryInsert(currentNode.getLeftChild(), newVal);
-            }
-        } else {
-            if (currentNode.getRightChild() == null) {
-                currentNode.addRightChild(new Node(newVal, currentNode));
-                System.out.printf("%d inserted right to parent : %d\n", newVal, currentNode.getCurrentNode());
+        System.out.println();
 
-            } else {
-                recursiveBinaryInsert(currentNode.getRightChild(), newVal);
-            }
-        }
+        System.out.print("LNR : ");
+        Node.inOrderBST(myTree);
+
+        System.out.println();
+
+        System.out.print("LRN : ");
+        Node.postOrderBST(myTree);
+
+        System.out.println();
+        System.out.println(Node.searchBST(myTree, 6));
+
+        myTree.calculateHeight();
+        System.out.println(myTree.getHeight());
+
     }
 
 }
--- Trees/Node.java
+++ Trees/Node.java
@@ -5,15 +5,18 @@ public class Node {
     private Node parent;
     private Node leftChild;
     private Node rightChild;
+    private int height = -1;
 
     public Node(int c, Node parent) {
         this.currentNode = c;
         this.parent = parent;
+        height++;
     }
 
     public Node(int c) {
         this.currentNode = c;
         this.parent = null;
+        height++;
     }
 
     public int getCurrentNode() {
@@ -40,11 +43,98 @@ public class Node {
         this.rightChild = rc;
     }
 
-    public static void inorder(Node node) {
+    public static void inOrderBST(Node node) {
         if (node == null)
             return;
-        inorder(node.getLeftChild());
+        inOrderBST(node.getLeftChild());
         System.out.print(node.getCurrentNode() + " ");
-        inorder(node.getRightChild());
+        inOrderBST(node.getRightChild());
+    }
+
+    public static void preOrderBST(Node node) {
+        if (node == null)
+            return;
+        System.out.print(node.getCurrentNode() + " ");
+        preOrderBST(node.getLeftChild());
+        preOrderBST(node.getRightChild());
+    }
+
+    public static void postOrderBST(Node node) {
+        if (node == null)
+            return;
+        postOrderBST(node.getLeftChild());
+        postOrderBST(node.getRightChild());
+        System.out.print(node.getCurrentNode() + " ");
+    }
+
+    public static boolean searchBST(Node node, int val) {
+        // return false if node is null
+        if (node == null)
+            return false;
+
+        // check if current node is the value
+        if (node.currentNode == val) {
+            System.out.println("Found!");
+            return true;
+        }
+
+        // traverse left or right based on value, using recursion
+        if (val < node.currentNode) {
+            return searchBST(node.leftChild, val);
+        } else {
+            return searchBST(node.rightChild, val);
+        }
+    }
+
+    public static Node InsertNodeBST(Node currentNode, int newVal) {
+        // initial case for the root insertion
+        if (currentNode == null) {
+            System.out.println("Root inserted : " + newVal);
+            currentNode = new Node(newVal);
+            return currentNode;
+        }
+
+        // traverse the tree to find the correct position
+        // insert left if newVal < currentNode value
+        if (newVal < currentNode.getCurrentNode()) {
+            // check if left child is null
+            if (currentNode.getLeftChild() == null) {
+                currentNode.addLeftChild(new Node(newVal, currentNode));
+                System.out.printf("%d inserted left to parent : %d\n", newVal, currentNode.getCurrentNode());
+            } else { // if not null, continue traversing left
+                InsertNodeBST(currentNode.getLeftChild(), newVal);
+            }
+        } else { // insert right if newVal >= currentNode value
+            // check if right child is null
+            if (currentNode.getRightChild() == null) {
+                currentNode.addRightChild(new Node(newVal, currentNode));
+                System.out.printf("%d inserted right to parent : %d\n", newVal, currentNode.getCurrentNode());
+
+            } else { // if not null, continue traversing right
+                InsertNodeBST(currentNode.getRightChild(), newVal);
+            }
+        }
+        return currentNode;
+    }
+
+    public void calculateHeight() {
+        int leftHeight = -1;
+        int rightHeight = -1;
+
+        if (this.leftChild != null) {
+            this.leftChild.calculateHeight();
+            leftHeight = this.leftChild.getHeight();
+        }
+
+        if (this.rightChild != null) {
+            this.rightChild.calculateHeight();
+            rightHeight = this.rightChild.getHeight();
+        }
+
+        this.height = Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public int getHeight() {
+        return this.height;
     }
 }
